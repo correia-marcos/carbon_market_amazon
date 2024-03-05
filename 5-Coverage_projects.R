de4b822be30b428cce9ce9ebe50078f2e5667861
Marcos Paulo <marcospaulorcorreia@gmail.com>
2024-03-04 21:41:39 -0300

    Improving code clarity, completing and adding analysis
    Former-commit-id: ec142218de378eba6d8316d4c2b5f575e81c5ffe
    Former-commit-id: aeb2cca11035f84e9fb0f0783d1860504b0f46a3

--- 5-Coverage_projects.R
+++ 5-Coverage_projects.R
@@ -1,12 +1,13 @@
 # =============================================================================
 # Dissertation: Can carbon market save the Amazon: Evidence from Brazil
 # =============================================================================
-# Goal: Collect 
+# Goal: Collect  land coverage information on the REDD+ properties 
 # 
-# We already create a dataset with virtually all 
-# 
-# This script was implemented to:
-#   1 - Create Figures for presentation
+# With the file 4-Merge_car.R we created a sf dataframe that contains all
+# properties that have REDD+ projects inside. Now we use this dataframe
+# and extract land coverage information of properties using all base of raster
+# files. Raster consists of MAPBIOMAS Land Cover and User in Brazil, version 8.
+# See: https://plataforma.brasil.mapbiomas.org
 # 
 # 
 # @Date: out 2023
@@ -19,88 +20,111 @@ rm(list = ls())
 library(groundhog)            # You need to have at least R version = 4.3.1
 
 # Required packages 
-pkgs <- c("ggplot2", "dplyr", "sf", "sp", "raster", "terra")
+pkgs <- c("ggplot2", "dplyr", "sf", "sp", "raster", "terra", "exactextractr",
+          "tidyverse", "furrr")
 
 # Load packages
 groundhog.library(pkgs, "2023-09-01")
 
-# Use this line to avoid geometries issues - allow for self-intersection
+# Run this line to avoid geometries issues - allow for self-intersection
 sf_use_s2(FALSE)
 
-# Load dataset of merged projects with CAR
-data_car <- st_read("Results/CAR_redd")
-data_projects <- sf::st_read("Results/base_afolu_complete")
+# Load the required datasets
+projects <- sf::st_read("Results/base_afolu_complete")
+legend <- read.csv("Data/Deforestation/Codigos-da-legenda-colecao-8.csv",
+                   sep = ';')
+data_car <- st_read("Results/CAR_redd_atlas/")
 
-# Load tiff file
-tiff_file <- "Data/Deforestation/brasil_coverage_legal_amazon_2000.tif"
-raster_data <- raster(tiff_file)
+# Change projects data frame column name
+colnames(projects)[1] <- 'id'
 
-# Função para calcular a porcentagem de cada tipo
-calculate_percentage <- function(project_geometry, raster_data) {
-  # Recortar o raster usando a geometria do projeto
-  raster_cropped <- crop(raster_data, extent(project_geometry))
+# Specify the directory path and regular expression pattern for matching files
+directory_path <- "Data/Deforestation/"
+pattern <- "^brasil_coverage_legal_amazon_[0-9]{4}\\.tif$"
+
+# List raster files matching the pattern in the directory and read them
+raster_files <- list.files(path = directory_path,
+                           pattern = pattern,
+                           full.names = TRUE)
+raster_list = lapply(raster_files, raster)
+
+# Function --------------------------------------------------------------------
+# written on: 12/01/2024
+# written by: Marcos Paulo
+# Input     : List of raster files and dataframe of land tenure spatial data
+# Output    : Dataframe with +- 40 columns with coverage land use, id and year
+# purpose   : Get the coverage land use of properties in IMAFLORA dataset
+# desc      : The function works in following way:
+# - for each raster file variable (one for each year) - i
+#   - for each property in the land tenure dataset - j
+#       - Do processing to get all the coverage usage of the properties in a 
+#       - new dataset, saving the ID of the property and the year of land use
+get_coverage_properties <- function(list_of_rast_files, data_land_tenure) {
+  # Get time of execution
+  start <- Sys.time()
+  # Create an empty data frame to store the data
+  coverage_car <-  data.frame()
   
-  # Calcular a área total do projeto
-  area_total <- st_area(project_geometry)
+  # Init the first loop
+  for (i in 1:length(list_of_rast_files)) {
+    cat(paste0( "Working on the following raster:", 
+                "\n",
+                filename(raster_list[[i]]),
+                "\n"))
+    
+    # Get the year of the raster
+    raster_name <- filename(list_of_rast_files[[i]])
+    year_string <- substr(raster_name,
+                          nchar(raster_name) - 7,
+                          nchar(raster_name) - 4)
+    year_num <- as.numeric(year_string)
+    
+    # Init the second loop 
+    for (j in 1:nrow(data_land_tenure)) {
+      # Take the j line of the land data
+      property <- data_land_tenure[j,]
+      
+      # Extract from raster only the important spatial area
+      extraction <- exact_extract(list_of_rast_files[[i]], property)
+      
+      # Create a data frame, sum the total area of each legend class, invert
+      # lines with columns, add ID and YEAR columns to it 
+      raster_property <- extraction[[1]] %>%
+        group_by(value) %>% 
+        summarise(class_id = sum(coverage_fraction)) %>%
+        tidyr::pivot_wider(names_from = value, values_from = class_id) %>% 
+        mutate(id = property$id) %>% 
+        mutate(year = year_num)
+      
+      # Add values to dataframe
+      coverage_car <- bind_rows(coverage_car, raster_property)
+    }
+  }
   
-  # Calcular a área de cada classe de cobertura
-  area_forest <- sum(values(raster_cropped) == 1)
-  area_non_forest_natural <- sum(values(raster_cropped) == 2)
-  area_agriculture <- sum(values(raster_cropped) == 3)
-  area_others_coverage <- sum(values(raster_cropped) %in% c(4, 5))
+  # Show time of code execution
+  print( Sys.time() - start )
   
-  # Calcular a porcentagem de cada classe de cobertura
-  perc_forest <- 100 * (area_forest / area_total)
-  perc_non_forest_natural <- 100 * (area_non_forest_natural / area_total)
-  perc_agriculture <- 100 * (area_agriculture / area_total)
-  perc_others_coverage <- 100 * (area_others_coverage / area_total)
-  
-  return(c(perc_forest, perc_non_forest_natural, perc_agriculture, perc_others_coverage))
+  # Return the final dataframe
+  return(coverage_car)
 }
 
+# Applying function
+land_coverage_car <- get_coverage_properties(list_of_rast_files = raster_list,
+                                             data_land_tenure = data_car)
 
-# Inicializar as colunas
-data_final$Forest <- NA
-data_final$Non_forest_natural <- NA
-data_final$Agriculture <- NA
-data_final$Others_coverage <- NA
+land_coverage_redd <- get_coverage_properties(list_of_rast_files = raster_list,
+                                              data_land_tenure = projects)
 
-# Loop sobre as geometrias dos projetos
-for (i in 1:nrow(data_final)) {
-  # Obter a geometria do projeto
-  project_geometry <- st_geometry(data_final[i, ])
-  
-  # Calcular as porcentagens
-  percentages <- calculate_percentage(project_geometry, raster_data)
-  
-  # Atribuir os resultados às colunas
-  data_final[i, c("Forest", "Non_forest_natural", "Agriculture", "Others_coverage")] <- percentages
-}
+# Save files
+write.csv2(land_coverage_car,
+           file = 'Results/Final_base/car_coverage.csv')
+write.csv2(land_coverage_redd,
+           file = 'Results/Final_base/projects_coverage.csv')
 
+# =====================
+# erros
+# =====================
 
+erros <- filter(coverage_redd, !is.na(X32))
 
-plot(raster_data)
-
-ext <- st_bbox(project_geometry)
-raster_cropped <- crop(raster_data, ext)
-
-
-raster_cropped <- ratify(raster_cropped)
-
-
-
-# 1. Cortar o arquivo tiff para cada geometria em data_final usando terra
-raster_data_cropped <- vector("list", length = nrow(data_final))
-
-for (i in 1:nrow(data_final)) {
-  project_geometry <- st_geometry(data_final[i, ])
-  
-  # Cortar o raster usando a geometria do projeto
-  raster_data_cropped[[i]] <- crop(raster_data, project_geometry)
-}
-
-
-
-# Calculate the percentage of each land cover type
-land_cover_percentages <- 
-  cellStats(legal_amazon_cropped, stat = "sum") / ncell(legal_amazon_cropped) * 100
+erros_car <- filter(data_car, id_rgst == unique(erros$id)) 
